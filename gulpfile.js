0e73d2eaa16fbce1e6db2be7661ea52c5f765286
Drew Devlin and Sarah Leahy <drew@>
2017-03-15 16:17:51 -0700

    add gulp-concat to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 var gulp = require("gulp");
 var source = require("vinyl-source-stream");
 var browserify = require("browserify");
-// var concat = require("gulp-concat");
+var concat = require("gulp-concat");
 // var uglify = require("gulp-uglify");
 // var utilities = require("gulp-util");
 var jshint = require("gulp-jshint");
 // var buildProduction = utilities.env.production;
 
-gulp.task('jsBrowserify' , function(){
-  return browserify({ entries: ['./js/hangman-interface.js', './js/hangman.js'] })
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('./build/js'));
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('jshint', function(){
